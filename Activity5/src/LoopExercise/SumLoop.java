9ee7fc5fa624348773944daf5249900d0ddc991b
Karren Grace A. Maniacop <kamaniacop@robinsonsbank.com.ph>
2025-12-04 15:29:36 +0800

    Add fifth activity"

---
+++ Activity5/src/LoopExercise/SumLoop.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package LoopExercise;
+
+/**
+ * 
+ */
+public class SumLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for (int i = 1; i<= 50; i++)
+		{
+			sum += i;
+		}
+			
+		System.out.println("Sum= " + sum);
+
+	}
+
+}
